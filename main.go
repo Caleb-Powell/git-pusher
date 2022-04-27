0c3848021c0be4357da0a9f637c0c05fb4b1b56e
caleb-powell <cappowell@gmail.com>
2022-04-27 06:38:10 -0400

    test

--- main.go
+++ main.go
@@ -1,25 +1,24 @@
 package main
 
 import (
-	"flag"
-	"io/ioutil"
-	"log"
 	"os"
 	"os/exec"
+
+	"github.com/Caleb-Powell/git-pusher/config"
+	"github.com/Caleb-Powell/git-pusher/command"
 )
 
+var (
+	conf = config.Config{}
+	cmd = command.GitCommand{}
+)
+
+func init() {
+	conf = config.GetConfig()
+}
+
 func main() {
-	pwd, err := os.Getwd()
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	path := flag.String("path", pwd, "This should be the directry where you hold all of your get repos. Default is the dir that it is running in.")
-
-	flag.Parse()
-
-	files, err := ioutil.ReadDir(*path)
+	files, err := os.ReadDir(conf.RepoPath)
 
 	if err != nil {
 		panic(err)
@@ -27,8 +26,11 @@ func main() {
 
 	for _, file := range files {
 		cmd := exec.Command("git", "push")
-		cmd.Dir = *path + "/" + file.Name()
+		cmd.Dir = conf.RepoPath + "/" + file.Name()
 		cmd.Run()
 	}
+}
 
+func getCommands() []*exec.Cmd {
+	if 
 }
